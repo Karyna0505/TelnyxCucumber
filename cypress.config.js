6078c831e6ebb6e93839840bf3217f7dbb44fa81
Karyna0505 <114803650+Karyna0505@users.noreply.github.com>
2023-01-17 13:23:00 +0100

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -11,7 +11,7 @@ module.exports = defineConfig({
   chromeWebSecurity: false,
   
   e2e: {
-    testIsolation: false,
+   
     specPattern: "**/*.feature",
     setupNodeEvents(on, config) {
       on("file:preprocessor", cucumber())
