9c2549f87157c5f14d3af1022265510f536a747a
Karyna0505 <karina.ivannikova96@gmail.com>
2023-01-27 13:51:32 +0100

    fixed

--- cypress/e2e/PagesObject/CalculatorBannerPage.cy.js
+++ cypress/e2e/PagesObject/CalculatorBannerPage.cy.js
@@ -4,8 +4,10 @@ const targetValue = 480000;
 const increment = 2000;
 const steps = (targetValue - currentValue) / increment;
 const arrows = '{rightarrow}'.repeat(steps); 
-const costTelnyx = 'div:nth-child(2) > div.sc-1a5981e5-14.bAmCql > span > span';
-const costTwilio = 'div.sc-1a5981e5-7.fgvOeg > div:nth-child(2) > span';
+const sliderOutbound = 'div:nth-child(4) .ant-slider-handle';
+const sliderInbound = 'div:nth-child(5) .ant-slider-handle';
+const costTelnyx = 'div>span>span';
+const costTwilio = 'div:nth-child(2)>div:nth-child(2)>span[class]';
 
 class SwithSave{
 
@@ -16,9 +18,9 @@ class SwithSave{
     }
 
     static switchSlider() {
-        cy.get('div:nth-child(4) > div.telnyx-slider > div > div.ant-slider-handle')
+        cy.get(sliderOutbound)
             .type(arrows);
-        cy.get('div:nth-child(5) > div.telnyx-slider > div > div.ant-slider-handle')
+        cy.get(sliderInbound)
             .type(arrows);       
     }
 
